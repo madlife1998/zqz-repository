4457b78d37517121061e3d312985802843c0587b
madlife1998 <18739866236@163.com>
2020-03-10 09:32:30 +0800

    second commit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ public class Main {
      * @return: void
      **/
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("second commit");
     }
 }
