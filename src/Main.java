6cf25b0c6bf5164536c2ff0029590ee163765862
madlife1998 <18739866236@163.com>
2020-03-10 09:17:54 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    /**
+     * @description: 
+     * @Date: 2020/3/5
+     * @param: args 
+     * @return: void
+     **/
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/Main2.java
@@ -0,0 +1,7 @@
+/**
+ * @author zqz
+ * @description 测试
+ * @date 2020/3/5
+ */
+public class Main2 {
+}
